866297da4b4a7ba27c28b11b2d31ff957cc6b1c0
ahrikri <ahrikri@gmail.com>
2016-07-09 18:40:14 +0300

    dist folder

--- react-interface/src/components/Header.js
+++ react-interface/src/components/Header.js
@@ -1,20 +1,17 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component/*, PropTypes */} from 'react'
 
 export default class Header extends Component {
     render() {
-        const { shopInfo, price, currencyText, paymoLogo } = this.props
 
         return (
-            <div className='paymo-widget__header'>
+            <div className='shareview-yandex__header'>
                 <h1>React shareview interface</h1>
             </div>
         )
     }
 }
 
-Header.propTypes = {
-    currencyText: PropTypes.string.isRequired,
-    paymoLogo: PropTypes.string.isRequired,
-    price: PropTypes.number.isRequired,
-    shopInfo: PropTypes.object.isRequired
-}
+/*
+ Header.propTypes = {
+
+ }*/
