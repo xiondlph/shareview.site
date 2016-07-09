d3432472f202dbea0555ef51172cb66eba182994
ahrikri <ahrikri@gmail.com>
2016-07-09 18:21:46 +0300

    header

--- react-interface/src/components/Header.js
+++ react-interface/src/components/Header.js
@@ -6,23 +6,7 @@ export default class Header extends Component {
 
         return (
             <div className='paymo-widget__header'>
-                <div className='pay-info pay-info_adapt-center'>
-                    <div className='pay-info__number'>
-                        <span className='pay-info__min-text'>{shopInfo.product}</span>
-                        <span className='pay-info__big-text'>{shopInfo.name}</span>
-                    </div>
-                    <div className='pay-info__price'>
-                        <span className='pay-info__min-text'>Сумма:</span>
-                        <span className='pay-info__big-text'>{price}{currencyText}</span>
-                    </div>
-                </div>
-                <div className='shop-info shop-info_left'>
-                    {shopInfo.logo &&
-                    <a href={shopInfo.url} className='logo'><img src={shopInfo.logo} alt={shopInfo.name} /></a>}
-                    {shopInfo.url &&
-                    <a href={shopInfo.url} className='back-shop'>назад в магазин</a>}
-                </div>
-                <div className='paymo-logo'><span className={'icon '+paymoLogo} /></div>
+                <h1>React shareview interface</h1>
             </div>
         )
     }
